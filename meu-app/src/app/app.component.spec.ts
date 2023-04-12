f14b650bc4560f7b930a9a7532f9706594590add
unknown <fgabi2302@gmail.com>
2023-04-12 16:37:51 -0300

    Entendendo estrutura do Laravel

--- meu-app/src/app/app.component.spec.ts
+++ meu-app/src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'meu-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('meu-app');
+    expect(app.nome).toEqual('meu-app');
   });
 
   it('should render title', () => {
