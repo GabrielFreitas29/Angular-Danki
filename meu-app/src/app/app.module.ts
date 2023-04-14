77397004c2876a8e93de8c8cb0b5080053214814
unknown <fgabi2302@gmail.com>
2023-04-14 10:15:27 -0300

    Criando um component

--- meu-app/src/app/app.module.ts
+++ meu-app/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SchoolComponent } from './school/school.component';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SchoolComponent
   ],
   imports: [
     BrowserModule,
