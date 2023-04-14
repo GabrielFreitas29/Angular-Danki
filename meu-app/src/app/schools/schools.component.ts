39599b8cf212678d773c9338131f21c8985fca7f
unknown <fgabi2302@gmail.com>
2023-04-14 10:52:16 -0300

    Trabalhando com estilo e diferenciando caminho com url e direto

--- meu-app/src/app/schools/schools.component.ts
+++ meu-app/src/app/schools/schools.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-schools',
-  templateUrl: './schools.component.html',
+  
+  /*
+  templateUrl:'/.schools.component.html', 
+  */
+
+  template: `
+    <app-school></app-school>
+    <app-school></app-school>`,
   styleUrls: ['./schools.component.css']
 })
 export class SchoolsComponent {
